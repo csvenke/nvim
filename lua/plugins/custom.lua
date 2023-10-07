1f1d60e2e504e4ed578ae880f093bdf3f712674d
Christian Svenkerud <csvenke@gmail.com>
2023-10-07 14:20:57 +0200

    feat: add neovim config

---
+++ lua/plugins/custom.lua
@@ -0,0 +1,70 @@
+return {
+  -- Language support
+  { import = "lazyvim.plugins.extras.lang.java" },
+  { import = "lazyvim.plugins.extras.lang.rust" },
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.docker" },
+
+  -- Utils
+  { import = "lazyvim.plugins.extras.util.project" },
+
+  -- Git
+  {
+    "sindrets/diffview.nvim",
+    opts = {
+      show_help_hints = false,
+      keymaps = {
+        view = {
+          { "n", "<leader>gd", "<cmd>DiffviewClose<cr>", remap = true, desc = "..." },
+        },
+        diff2 = {
+          { "n", "<leader>gd", "<cmd>DiffviewClose<cr>", remap = true, desc = "..." },
+        },
+        file_panel = {
+          { "n", "<leader>gd", "<cmd>DiffviewClose<cr>", remap = true, desc = "..." },
+        },
+      },
+    },
+    keys = {
+      { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "diff" },
+    },
+  },
+
+  -- Formatting
+  { import = "lazyvim.plugins.extras.formatting.conform" },
+  { import = "lazyvim.plugins.extras.formatting.prettier" },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        ["javascript"] = { { "prettierd", "prettier" } },
+        ["javascriptreact"] = { { "prettierd", "prettier" } },
+        ["typescript"] = { { "prettierd", "prettier" } },
+        ["typescriptreact"] = { { "prettierd", "prettier" } },
+        ["vue"] = { { "prettierd", "prettier" } },
+        ["css"] = { { "prettierd", "prettier" } },
+        ["scss"] = { { "prettierd", "prettier" } },
+        ["less"] = { { "prettierd", "prettier" } },
+        ["html"] = { { "prettierd", "prettier" } },
+        ["json"] = { { "prettierd", "prettier" } },
+        ["jsonc"] = { { "prettierd", "prettier" } },
+        ["yaml"] = { { "prettierd", "prettier" } },
+        ["markdown"] = { { "prettierd", "prettier" } },
+        ["markdown.mdx"] = { { "prettierd", "prettier" } },
+        ["graphql"] = { { "prettierd", "prettier" } },
+      },
+    },
+  },
+
+  -- Personality
+  { import = "lazyvim.plugins.extras.ui.mini-animate" },
+  { "projekt0n/github-nvim-theme" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
