652e66bf5ef70fd5a232108a9bf8d13de835f8b9
Christian Svenkerud <csvenke@gmail.com>
2023-10-26 17:13:33 +0200

    feat: update

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -24,6 +24,10 @@ return {
   {
     "folke/edgy.nvim",
     event = "VeryLazy",
+    init = function()
+      vim.opt.laststatus = 3
+      vim.opt.splitkeep = "screen"
+    end,
     opts = {
       left = {
         {
